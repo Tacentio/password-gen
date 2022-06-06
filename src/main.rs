f2cb51e6b11b5ba80b197e883186284ff16f020e
Daniel Moore <daniel_moore@trendmicro.com>
2022-06-06 10:53:53 +1000

    remove binary

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use password_gen::{PasswordGenerator, PasswordOptions, CharSet};
-
-fn main() {
-    let mut generator = PasswordGenerator::new();
-    let options = PasswordOptions::new(10, CharSet::Ascii);
-    let password = generator.generate_password(&options);
-    println!("{password}");
-}
